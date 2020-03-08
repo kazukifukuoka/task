8467946d5813889e83f70c444a3496e82a08c906
福岡和樹 <kazuki@mba.local>
2020-03-08 13:05:50 +0900

    q11-q15

--- task.rb
+++ task.rb
@@ -90,19 +90,19 @@ end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
+
+  # 以下に回答を記載
   puts "ユーザーの趣味一覧"
   sports.flatten.uniq.each.with_index(1) do |sport,i|
     puts "No#{i} #{sport}"
   end
-  # 以下に回答を記載
-
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
-  p data[:user][:name]
-  # 以下に回答を記載
 
+  # 以下に回答を記載
+  p data[:user][:name]
 end
 
 def q13
