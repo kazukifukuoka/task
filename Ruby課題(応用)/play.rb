f6860864988ebd4432198e56be069f2889c5fd60
福岡和樹 <kazuki@mba.local>
2020-03-15 14:22:19 +0900

    lesson1

--- Ruby課題(応用)/play.rb
+++ Ruby課題(応用)/play.rb
@@ -2,7 +2,7 @@ require "./brave.rb"
 require "./monster.rb"
 
 brave = Brave.new(hp: 500, offense: 150, defense: 100)
-monster = Monster.new(hp: 400, offense: 100, defense: 80)
+monster = Monster.new(hp: 250, offense: 200, defense: 100)
 
 brave.status
 monster.status
